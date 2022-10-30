2ed636a38a9bc9aa36e36295bc58886625e24c3c
busra <busrasariaslan60@gmail.com>
2022-10-30 18:55:35 +0300

    My first simple app that I wrote with Swift

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,11 +9,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headLabel: UILabel!
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func sumClicked(_ sender: Any) {
+        
+        let firstNumber = Int(firstText.text!)!
+        let secondNumber = Int(secondText.text!)!
+        
+        let result = firstNumber + secondNumber
+        
+        resultLabel.text = String(result)
+    }
+    
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        
+        let firstNumber = Int(firstText.text!)!
+        let secondNumber = Int(secondText.text!)!
+        
+        let result = firstNumber - secondNumber
+        
+        resultLabel.text = String(result)
+        
+    }
+    
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        
+        let firstNumber = Int(firstText.text!)!
+        let secondNumber = Int(secondText.text!)!
+        
+        let result = firstNumber * secondNumber
+        
+        resultLabel.text = String(result)
+        
+    }
+    
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        
+        let firstNumber = Int(firstText.text!)!
+        let secondNumber = Int(secondText.text!)!
+        
+        let result = firstNumber / secondNumber
+        
+        resultLabel.text = String(result)
+        
+    }
+    
+    
+    
+    
 }
 
